80d1738cba166088c17a7b4f4f12d7d83d7e82cc
Luis Campos R <luiscmpsr@gmail.com>
2025-09-21 21:24:20 -0600

    Cambios en los urls

--- django_proyecto/urls.py
+++ django_proyecto/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from views import buscar_view, solicitud_view
+from views import buscar_view, solicitud_view, crear_solicitud_view
 
 urlpatterns = [
     path("", buscar_view, name="buscador"),
 path('solicitud/', solicitud_view, name='solicitud'),
+path('crear_solicitud/', crear_solicitud_view, name='crear_solicitud'),
 ]
